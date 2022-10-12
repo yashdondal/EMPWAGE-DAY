8889a1f4cfae849156be17bca68a594d75db154e
yashdondal <yashdondal21@gmail.com>
2022-10-12 19:44:02 +0530

    Employee Attendence

---
+++ EmployeeAttendence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_EmpWage
+{
+    public class EmployeeAttendence
+    {
+        public void CheckEmployeeAttendance()
+        {
+            int Is_FullTime = 1;
+            Random random = new Random();
+            int emplCheck = random.Next(0, 2);
+
+            if (emplCheck == 1)
+            {
+                Console.WriteLine("Empolyee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}

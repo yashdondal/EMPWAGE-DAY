8dfa9e35cfefd484726dee298d35a299da7880e2
yashdondal <yashdondal21@gmail.com>
2022-10-12 19:41:59 +0530

    greeting

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day_8_EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Empwage");
+            
+
+            EmployeeAttendence employeeAttendence = new EmployeeAttendence();
+            employeeAttendence.CheckEmployeeAttendance();
+
+            CalculateEmpWage calculateEmpWage = new CalculateEmpWage();
+            calculateEmpWage.ToCalculateEmpWage();
+
+            PartTimeEmpwage partTimeEmpWage = new PartTimeEmpwage();
+            partTimeEmpWage.ToCalculateEmpWage();
+
+            SwitchCase sw = new SwitchCase();
+            sw.ToCalculateEmpWage();
+
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.ToCalculateEmpWage();
+
+            TotalEmpWage totalEmpWage = new TotalEmpWage();
+            totalEmpWage.ToCalculateEmpWage();
+        }
+    }
+}

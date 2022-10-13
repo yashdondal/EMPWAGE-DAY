cbb36a70c8e64a3d8fc2de0e13e570b9809b4ae4
yashdondal <yashdondal21@gmail.com>
2022-10-13 18:26:17 +0530

    total wage

---
+++ MultipleCompany.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_EmpWage
+{
+    public class MultipleCompany
+    {
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+
+        public int wages(string company, int EmpRateprHr, int NumWorkingDay, int MaxHrPrMonth)
+        {
+            //variable
+            int emphr = 0;
+            int totalemphr = 0;
+            int totalworkingday = 0;
+            Console.WriteLine("monthly wages of employee");
+            Console.WriteLine("------------------------------");
+            while (totalemphr <= MaxHrPrMonth && totalworkingday < NumWorkingDay)
+            {
+                totalworkingday++;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case FULL_TIME:
+                        Console.WriteLine("Employee is FullTime");
+                        emphr = 8;
+                        break;
+                    case PART_TIME:
+                        Console.WriteLine("Employee is PartTime");
+                        emphr = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        emphr = 0;
+                        break;
+
+                }
+                totalemphr += emphr;
+                Console.WriteLine("days {0}:", totalworkingday + " emp hour " + emphr);
+
+            }
+            Console.WriteLine("total Employee hour is {0}", totalemphr);
+
+            int totalempwage = totalemphr * EmpRateprHr;
+            Console.WriteLine("total employee wage for company {0} and total Employee wage {1} ", company, totalempwage);
+            return totalempwage;
+        }
+    }
+}
---
+++ TotalWage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_EmpWage
+{
+    public class TotalWage
+    {
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+
+        private string company;
+        private int EmpRateprhr;
+        private int NumWorkingDay;
+        private int MaxHrPrMonth;
+        private int totalEmpWage;
+        public TotalWages(string company, int EmpRateprhr, int NumWorkingDay, int MaxHrPrMonth)
+        {
+            this.company = company;
+            this.EmpRateprhr = EmpRateprhr;
+            this.NumWorkingDay = NumWorkingDay;
+            this.MaxHrPrMonth = MaxHrPrMonth;
+        }
+
+        public void ComputeEmpWage()
+        {
+            //variable
+            int emphr = 0;
+            int totalemphr = 0;
+            int totalworkingday = 0;
+
+            while (totalemphr <= MaxHrPrMonth && totalworkingday < NumWorkingDay)
+            {
+                totalworkingday++;
+                Console.WriteLine("monthly wages of employee");
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case FULL_TIME:
+                        Console.WriteLine("Employee is FullTime");
+                        emphr = 8;
+                        break;
+                    case PART_TIME:
+                        Console.WriteLine("Employee is PartTime");
+                        emphr = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        emphr = 0;
+                        break;
+                }
+
+                totalemphr += emphr;
+                Console.WriteLine("total days {0}:", totalworkingday + " emp hour " + emphr);
+
+            }
+            int totalempwage = totalemphr * this.EmpRateprhr;
+            Console.WriteLine("total employee wage : {0}", totalempwage);
+        }
+        public string toString()
+        {
+            return "total emp wage for company:" + this.company + " is" + totalEmpWage;
+        }
+    }
+}
